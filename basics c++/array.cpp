d840a84d7864cce56f2f2112bba8b8869c05e65b
Gaurav <royg48848@gmail.com>
2025-12-26 23:57:07 +0530

    linux branch first array

---
+++ basics c++/array.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int barr[4] = {1,2,3,4};
+    for (int i = 0; i < 4; i++) {
+        cout << "Element at index " << i << ": " << barr[i] << endl;
+    }
+    return 0;
+}
